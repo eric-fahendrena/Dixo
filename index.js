685915f4fc0f9f2824d69b4561f67dffd39bfc83
Eric Fahendrena <ericfahendrena29@gmail.com>
2024-05-23 19:11:55 +0300

    send static files

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html');
+  res.sendFile('public/index.html');
 });
 
 app.get('/api/data', (req, res) => {
