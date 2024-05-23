ab51d6cbc8d334cc33f1e0efe9c13a4cb5f05b3f
Eric Fahendrena <ericfahendrena29@gmail.com>
2024-05-23 19:41:13 +0300

    fix path not found

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/api/data', (req, res) => {
